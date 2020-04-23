87cfb335bb02667b7d9611498696b908027a30d2
uttank <uttank@snu.ac.kr>
2020-04-23 16:27:19 +0900

    Todo 표시 하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 function App() {
-  return <TodoTemplate>Todo App 을 만들자!</TodoTemplate>;
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정관리 앱을 만들어 보기',
+      checked: false,
+    },
+  ]);
+  return (
+    <TodoTemplate>
+      <TodoInsert />
+      <TodoList todos={todos} />
+    </TodoTemplate>
+  );
 }
 
 export default App;
