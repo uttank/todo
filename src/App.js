e666444a036f95315414b4fd80e57bb292c5ae85
uttank <uttank@snu.ac.kr>
2020-04-23 13:44:49 +0900

    todo 1

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+
+function App() {
+  return <TodoTemplate>Todo App 을 만들자!</TodoTemplate>;
+}
+
+export default App;
