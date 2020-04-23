45461303f01f4d6667b37daa6ee4eec0fbc00281
uttank <uttank@snu.ac.kr>
2020-04-23 17:49:58 +0900

    performance

--- src/App copy.js
+++ src/App copy.js
@@ -3,21 +3,26 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function createBulkTodos() {
-  const array = [];
-  for (let i = 1; i <= 2500; i++) {
-    array.push({
-      id: i,
-      text: `할 일 ${i}`,
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트의 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '컴포넌트 스타일링해 보기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '일정관리 앱을 만들어 보기',
       checked: false,
-    });
-  }
-  return array;
-}
-const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
+    },
+  ]);
 
-  const nextId = useRef(1501);
+  const nextId = useRef(4);
 
   const onInsert = useCallback(
     (text) => {
@@ -55,6 +60,6 @@ const App = () => {
       <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
-};
+}
 
 export default App;
