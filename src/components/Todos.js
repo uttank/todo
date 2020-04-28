20468c2748774ea72deffb31f534dbe021751e46
uttank <uttank@snu.ac.kr>
2020-04-28 10:35:15 +0900

    redux 로 counter container 처리

---
+++ src/components/Todos.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const TodoItem = ({ todo, onToggle, onRemove }) => {
+  return (
+    <div>
+      <input type="checkbox" />
+      <span>예제 텍스트</span>
+      <button>삭제</button>
+    </div>
+  );
+};
+
+const Todos = ({ input, todos, onChangeInput, onToggle, onRemove }) => {
+  const onSubmit = (e) => {
+    e.preventDefautl();
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
+    </div>
+  );
+};
+
+export default Todos;
